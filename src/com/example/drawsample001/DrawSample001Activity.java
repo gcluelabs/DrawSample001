48d5444adf9654db12b3d22d8466410e3600774e
gcluelabs <gcluelabs@gclue.jp>
2013-05-25 15:54:30 +0900

    コメント削除

--- src/com/example/drawsample001/DrawSample001Activity.java
+++ src/com/example/drawsample001/DrawSample001Activity.java
@@ -13,44 +13,29 @@ public class DrawSample001Activity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		// �N���X�̃C���X�^���X�𐶐�
 		MyView mView = new MyView( this );
 
-		// View�ɐݒ�
 		setContentView( mView );
 	}
 }
 
-/**
- * �`��p�̃N���X�B
- */
 class MyView extends View {
 
-	/**
-	 * �R���X�g���N�^�B
-	 * @param context �R���e�L�X�g
-	 */
 	public MyView( Context context ) {
 		super( context );
 		setFocusable( true );
 	}
 
-	/**
-	 * �`�揈�����s���B
-	 */
 	@Override
 	protected void onDraw( Canvas canvas ) {
 		super.onDraw( canvas );
 
-		// �w�i�F��ݒ�
 		canvas.drawColor( Color.BLUE );
 
-		// �`�悷�邽�߂̐�̐F��ݒ�
 		Paint mPaint = new Paint();
 		mPaint.setStyle( Paint.Style.FILL );
 		mPaint.setARGB( 255, 255, 255, 100 );
 
-		// ��ŕ`��
 		canvas.drawLine( 0, 0, 50, 50, mPaint );
 	}
 }
